615a67bbb69ce627f957faf8c42de634bbffc37e
artem108 <artem.arshaniy108@gmail.com>
2017-09-13 22:59:13 +0300

    added window for game in dialog - dosent working)

--- src/Main/Menu/TalkMe/TalkMe.js
+++ src/Main/Menu/TalkMe/TalkMe.js
@@ -2,41 +2,53 @@ import React, { Component } from 'react'
 import './TalkMe.css'
 import Questions from './Questions';
 import Answers from './Answers'
+import UUU from './UUUneuralNetwork/UUU'
 
 class TalkMe extends Component {
   constructor(props) {
     super(props);
     this.state = {
       term: '',
-      items: []
+      items: [],
+      play: false
     };
   }
 
   onChange = (event) => {
     this.setState({ term: event.target.value });
   }
+  Play() {
+      this.setState({ play: !this.state.play});
+  }
 
   onSubmit = (event) => {
     event.preventDefault();
     this.setState({
       term: '',
-      items: [...this.state.items, this.state.term]
+      items: [...this.state.items, this.state.term],
     });
   }
 
   render() {
-
+    let player
+    if (this.state.play) {
+      player = <UUU />
+    }
     return (
       <section className='talkMe-container'>
         <div className='dialog-item'>
           <Answers items={this.state.items} />
           <Questions  items={this.state.items} />
+          {player}
         </div>
           <div className='controller-dialog '>
           <form onSubmit={this.onSubmit}>
          <input value={this.state.term} onChange={this.onChange} className="dialog-input-send"/>
          <button className='dialog-btn-send'><i className="fa fa-paper-plane" aria-hidden="true"></i></button>
        </form>
+       <div>
+         <button className='dialog-btn-send' onClick={this.Play.bind(this)}>Play</button>
+       </div>
         </div>
         </section>
     );
---
+++ src/Main/Menu/TalkMe/UUUneuralNetwork/UUU.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+class UUU extends Component {
+
+render() {
+  return (
+    <section>
+    <div id="container">
+      <canvas id="sketchpad" >Sorry, your browser is not supported.</canvas>
+      <canvas id="thumbnail">Sorry, your browser is not supported.</canvas>
+      <div></div>
+      <div ></div>
+  </div>
+  <button type="button" id="sketchClearButton">Clear</button>
+  <button type="button" id="sketchRecogniseButton">Recognise</button>
+</section>
+  )
+
+}
+
+}
+export default UUU
