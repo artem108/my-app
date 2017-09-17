abffc061d746c30ca3125ef5751b5401801f9816
artem108 <artem.arshaniy108@gmail.com>
2017-09-17 22:22:10 +0300

    Added part in news and create new elements there

--- src/Main/Menu/News/News.js
+++ src/Main/Menu/News/News.js
@@ -1,39 +1,114 @@
 import React, { Component } from 'react'
-// import './News.css'
-import Header from '../../Header/Header'
+import './News.css'
 
 class News extends Component {
-  render() {
-    return (
-      <section>
-        <div className='News-img'>
 
-        </div>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-          Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-           Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-          Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-             Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-              Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-               Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-              Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                 Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                  Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                   Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                  Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                     Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        </section>
+  render() {
+
+    return (
+      <div className="wrapper">
+        <header className="header-title">
+          <h1>a minute of self-expression with me</h1>
+          <article className="aboute-news">
+                <p>I think,  it would be nice for everyone, to learn something new for themselves,
+                   or to repeat already familiar material about the world in which we live.
+                   I deal with such terrible things in my free time and if you are a space lover like me,
+                  I would like to share this with you.</p>
+          </article>
+        </header>
+        <section className="main-news">
+            <section className="main-news-box">
+                <header className="header-news-title">
+                  <p>Cassini the grand finale</p>
+                </header>
+                <section className="news-image">
+                </section>
+                <section className="short-description">
+                  <p>The Cassini–Huygens mission was a collaboration between NASA,
+                     and the Italian Space Agency (ASI) to send a probe to study the planet Saturn completed his mission and
+                     burned in the atmosphere of Saturn.</p>
+                </section>
+                <div  className="read-more">
+                  <a href="#">Read more...</a>
+                </div>
+              </section>
+            <section className="main-news-box">
+                <header className="header-news-title">
+                  <p>Little about Spacetime</p>
+                </header>
+                <section className="news-image news-image-2">
+                </section>
+                <section className="short-description">
+                  <p>In physics, spacetime is any mathematical model that fuses the three dimensions of space and the one
+                    dimension of time into a single 4‑dimensional continuum. Today, let's talk about this. </p>
+                </section>
+                <div  className="read-more">
+                  <a href="#">Read more...</a>
+                </div>
+            </section>
+            <section className="main-news-box">
+                <header className="header-news-title">
+                  <p>New flashes on the Sun. WHY?</p>
+                </header>
+                <section className="news-image news-image-3">
+                </section>
+                <section className="short-description">
+                  <p> It lasts for several minutes, releasing billions of megatons of energy in the TNT equivalent.
+                    Let's find out why this is happening and why humanity will not die from this yet.</p>
+                </section>
+                <div  className="read-more">
+                  <a href="#">Read more...</a>
+                </div>
+            </section>
+          </section>
+          <section className="main-news">
+              <section className="main-news-box">
+                  <header className="header-news-title">
+                    <p>Cassini the grand finale</p>
+                  </header>
+                  <section className="news-image">
+                  </section>
+                  <section className="short-description">
+                    <p>The Cassini–Huygens mission was a collaboration between NASA,
+                       and the Italian Space Agency (ASI) to send a probe to study the planet Saturn completed his mission and
+                       burned in the atmosphere of Saturn.</p>
+                  </section>
+                  <div  className="read-more">
+                    <a href="#">Read more...</a>
+                  </div>
+                </section>
+              <section className="main-news-box">
+                  <header className="header-news-title">
+                    <p>Little about Spacetime</p>
+                  </header>
+                  <section className="news-image news-image-2">
+                  </section>
+                  <section className="short-description">
+                    <p>In physics, spacetime is any mathematical model that fuses the three dimensions of space and the one
+                      dimension of time into a single 4‑dimensional continuum. Today, let's talk about this. </p>
+                  </section>
+                  <div  className="read-more">
+                    <a href="#">Read more...</a>
+                  </div>
+              </section>
+              <section className="main-news-box">
+                  <header className="header-news-title">
+                    <p>New flashes on the Sun. WHY?</p>
+                  </header>
+                  <section className="news-image news-image-3">
+                  </section>
+                  <section className="short-description">
+                    <p> It lasts for several minutes, releasing billions of megatons of energy in the TNT equivalent.
+                      Let's find out why this is happening and why humanity will not die from this yet.</p>
+                  </section>
+                  <div  className="read-more">
+                    <a href="#">Read more...</a>
+                  </div>
+              </section>
+            </section>
+          </div>
+
+
     );
   }
 }
--- src/Main/Menu/TalkMe/TalkMe.js
+++ src/Main/Menu/TalkMe/TalkMe.js
@@ -2,23 +2,18 @@ import React, { Component } from 'react'
 import './TalkMe.css'
 import Questions from './Questions';
 import Answers from './Answers'
-import UUU from './UUUneuralNetwork/UUU'
 
 class TalkMe extends Component {
   constructor(props) {
     super(props);
     this.state = {
       term: '',
-      items: [],
-      play: false
+      items: []
     };
   }
 
   onChange = (event) => {
     this.setState({ term: event.target.value });
-  }
-  Play() {
-      this.setState({ play: !this.state.play});
   }
 
   onSubmit = (event) => {
@@ -30,25 +25,17 @@ class TalkMe extends Component {
   }
 
   render() {
-    let player
-    if (this.state.play) {
-      player = <UUU />
-    }
     return (
       <section className='talkMe-container'>
         <div className='dialog-item'>
           <Answers items={this.state.items} />
           <Questions  items={this.state.items} />
-          {player}
         </div>
           <div className='controller-dialog '>
           <form onSubmit={this.onSubmit}>
          <input value={this.state.term} onChange={this.onChange} className="dialog-input-send"/>
          <button className='dialog-btn-send'><i className="fa fa-paper-plane" aria-hidden="true"></i></button>
        </form>
-       <div>
-         <button className='dialog-btn-send' onClick={this.Play.bind(this)}>Play</button>
-       </div>
         </div>
         </section>
     );
